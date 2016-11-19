2ab0c1f8856bf6eaac520f371e400a5600d2c172
goldstarwebs <donia@goldstarwebs.com>
2016-11-19 15:09:29 -0600

    Experimenting with promises

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+// on Heroku, use environment variables; if on localhost, use config-private file that does not get deployed
 var configfile = (process.env.consumer_key === undefined) ? './config-private.js' : './config.js';
 var config = require(configfile);
 
@@ -40,34 +41,50 @@ function getRandomDateString(year, month, day) {
 }
 
 
+var getPhotoFromNasa = function() {
+  return new Promise(function(resolve, reject) {
+    // date  YYYY-MM-DD  The date of the APOD image to retrieve; defaults to today
+    // APOD has images back to 1995, but higher quality images are more recent
+    // generate random date between now and 2005
+
+    // month is 0 indexed
+    var shortrandomdate = getRandomDateString(2005, 0, 1);
+    var imgurl = "http://api.nasa.gov/planetary/apod?api_key=" + config.nasa_api_key + "&date=" + shortrandomdate;
+
+    request
+      .get(imgurl)
+      .end(function(nasaerror, nasaresult) {
+        // if http request to NASA passes
+        if (nasaresult) {
+console.log(nasaresult);
+          // Set options for image_downloader
+          var options = {
+            url: nasaresult.body.url,
+            dest: 'tmp'
+          };
+
+          image_downloader(options);
+        } 
+        else {
+          reject(nasaerror);
+        }
+
+        resolve(nasaresult.body);
+      });
+  })
+}
+
 function tweetPhoto() {
 
-  // date  YYYY-MM-DD  The date of the APOD image to retrieve; defaults to today
-  // APOD has images back to 1995, but higher quality images are more recent
-  // generate random date between now and 2005
+  getPhotoFromNasa().then(function(photoinfo) {
+    console.log(photoinfo);
+  }).catch(function(error) {
+//    console.log('Error-- ', error.message);
+  });
 
-  // month is 0 indexed
-  var shortrandomdate = getRandomDateString(2005, 0, 1);
 
-  var imgurl = "https://api.nasa.gov/planetary/apod?api_key=" + config.nasa_api_key + "&date=" + shortrandomdate;
 
-  request
-    .get(imgurl)
-    .end(function(ajaxerror0, ajaxresult0) {
-      if (ajaxresult0) {
-        var copyrighttext = (ajaxresult0.body.copyright !== undefined) ? ajaxresult0.body.copyright : "Public Domain";
-
-        // Download to a directory and save with the original filename 
-        var options = {
-          url: ajaxresult0.body.url,
-          dest: 'tmp',
-          done: function(err, filename, image) {
-            if (err) {
-              throw err;
-            }
-            // console.log('File saved to', filename);
-
-            var filePath = filename;
+/*            var filePath = ajaxphoto.url;
             T.postMediaChunked({
               file_path: filePath
             }, function(err, data, response) {
@@ -78,7 +95,9 @@ function tweetPhoto() {
                 .get(quoteurl)
                 .end(function(ajaxerror, ajaxresult) {
                   if (ajaxresult) {
-                    var tweettext = ajaxresult.body.quoteText + " -" + ajaxresult.body.quoteAuthor + "\nImage Credits: " + copyrighttext;
+                    var copyrighttext = (ajaxphoto.copyright !== undefined) ? ajaxphoto.copyright : "Public Domain";
+
+                    var tweettext = ajaxresult.body.quoteText + " -" + ajaxphoto.quoteAuthor + "\nImage Credits: " + copyrighttext;
                     //check to see if full tweet text is going to be over 140 characters
 
 console.log(tweettext.length);
@@ -109,14 +128,8 @@ console.log(tweettext);
                     console.log("There was an Ajax quote error.");
                   }
                 });
-            })
-          }
-        };
-        image_downloader(options);
-      } else {
-        console.log("There was an Ajax photo error.");
-      }
-    });
+            })*/
+
 }
 
 tweetPhoto();
