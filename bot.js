7d2f0cb2197ed7f0bfff2891d84708cc9f1cffc7
goldstarwebs <donia@goldstarwebs.com>
2016-11-19 12:29:32 -0600

    Create getRandomDateString function

--- bot.js
+++ bot.js
@@ -1,19 +1,24 @@
+var config = require('./config-private.js');
+
+var image_downloader = require('image-downloader');
 var request = require('superagent');
 var Twit = require('twit');
-var image_downloader = require('image-downloader');
-
-var config = require('./config.js');
 
 var T = new Twit(config);
 
-function tweetPhoto() {
+/**
+* Calculates a random date between today and the date passed in
+* @method getRandomDateString
+* @param {number} year
+* @param {number} month
+* @param {number} day
+* @param {requestCallback} callback - The callback that handles the response.
+* @return {String} some bool
+*/
 
-  // date  YYYY-MM-DD  The date of the APOD image to retrieve; defaults to today
-  // APOD has images back to 1995, but higher quality images are more recent
-  // generate random date between now and 2005
-
+function getRandomDateString(year, month, day) {
   var todaysdate = new Date();
-  var earliestdate = new Date(2005, 0, 1);
+  var earliestdate = new Date(year, month, day);
 
   const millisecondsinday = 24*60*60*1000;
 
@@ -23,12 +28,25 @@ function tweetPhoto() {
   var elapseddays = (todayseconds - earliestseconds)/millisecondsinday;
 
   // now randomly pick a number between 1 and elapseddays
-  var randomday = Math.floor((Math.random() * elapseddays) + 1);
-  var randomseconds = earliestseconds + (randomday*millisecondsinday);
+  var randomnum = Math.floor((Math.random() * elapseddays) + 1);
+  var randomseconds = earliestseconds + (randomnum*millisecondsinday);
   var randomdate = new Date(randomseconds);
 
   var datestringarray = randomdate.toISOString().split('T');
-  var shortrandomdate = datestringarray[0];
+  
+  return datestringarray[0];
+
+}
+
+
+function tweetPhoto() {
+
+  // date  YYYY-MM-DD  The date of the APOD image to retrieve; defaults to today
+  // APOD has images back to 1995, but higher quality images are more recent
+  // generate random date between now and 2005
+
+  // month is 0 indexed
+  var shortrandomdate = getRandomDateString(2005, 0, 1);
 
   var imgurl = "https://api.nasa.gov/planetary/apod?api_key=" + config.nasa_api_key + "&date=" + shortrandomdate;
 
@@ -36,24 +54,23 @@ function tweetPhoto() {
     .get(imgurl)
     .end(function(ajaxerror0, ajaxresult0) {
       if (ajaxresult0) {
-        var imagelocation = ajaxresult0.body.url;
-        var copyrighttext = "";
+        var copyrighttext = (ajaxresult0.body.copyright !== undefined) ? ajaxresult0.body.copyright : "Public Domain";
 
-        if (ajaxresult0.body.copyright !== undefined) {
+/*        if (ajaxresult0.body.copyright !== undefined) {
           copyrighttext = ajaxresult0.body.copyright;
         } else {
           copyrighttext = "Public Domain";
         }
-
+*/
         // Download to a directory and save with the original filename 
         var options = {
-          url: imagelocation,
+          url: ajaxresult0.body.url,
           dest: 'tmp',
           done: function(err, filename, image) {
             if (err) {
               throw err;
             }
-             console.log('File saved to', filename);
+            // console.log('File saved to', filename);
 
             var filePath = filename;
             T.postMediaChunked({
@@ -69,20 +86,30 @@ function tweetPhoto() {
                     var tweettext = ajaxresult.body.quoteText + " -" + ajaxresult.body.quoteAuthor + "\nImage Credits: " + copyrighttext;
                     //check to see if full tweet text is going to be over 140 characters
 
+console.log(tweettext.length);
+
+if (tweettext.length > 140) {
+
+}
+
+console.log(tweettext);
+
+
+
                     var idstring = data.media_id_string;
                     var params = {
                       status: tweettext,
                       media_ids: [idstring]
                     };
 
-                    T.post('statuses/update', params, function(twittererror, tweet, twitterresponse) {
+                    /*T.post('statuses/update', params, function(twittererror, tweet, twitterresponse) {
                       if (twitterresponse) {
                         console.log('Tweet was posted');
                       }
                       if (twittererror) {
                         console.log('Twitter returned this error: ', twittererror);
                       }
-                    });
+                    });*/
                   } else {
                     console.log("There was an Ajax quote error.");
                   }
@@ -97,14 +124,14 @@ function tweetPhoto() {
     });
 }
 
-//tweetPhoto();
+tweetPhoto();
 
 // post every 8 hours
-setInterval(function() {
+/*setInterval(function() {
   try {
     tweetPhoto();
   }
   catch (e) {
     console.log(e);
   }
-}, 8*60*60*1000);
+}, 8*60*60*1000);*/
